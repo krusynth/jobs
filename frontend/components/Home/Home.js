22536cb6d4329b0fbd0c1f56431b786fd2f9ffae
Bill Hunt <bill@krues8dr.com>
2019-04-14 21:01:29 -0400

    Fix up homepage content.

--- frontend/components/Home/Home.js
+++ frontend/components/Home/Home.js
@@ -6,26 +6,47 @@ import UserHome from './UserHome';
 export default class Home extends Component {
   renderHome() {
     return (<div>
-      <h1>Welcome to Jobs</h1>
-
+      <p className="tagline">
+        Hunting for a job is hard. Let's make it work for you!
+      </p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et diam in erat congue aliquam. Nulla eu mollis quam, sed dictum sem. Donec justo augue, scelerisque ut nulla ut, sollicitudin aliquam quam. Sed vitae auctor augue, at cursus velit. Quisque dignissim nisi vitae urna scelerisque, eleifend ultricies justo dapibus. Mauris venenatis erat aliquam nisl eleifend, eu pharetra neque pellentesque. Pellentesque dolor tellus, eleifend sed nulla sollicitudin, commodo placerat tellus. Aliquam porttitor diam felis, eu facilisis urna faucibus at. Integer tincidunt, orci in vulputate pretium, ligula metus condimentum purus, sed egestas nisl mi at dui. Aenean vulputate dictum laoreet. Donec sed molestie massa. Ut mollis nisl nec erat tempus, nec posuere nunc laoreet. Nam aliquam orci vel ullamcorper pellentesque. Maecenas consequat hendrerit turpis, at consequat mi. Donec imperdiet libero turpis, vitae volutpat nibh ullamcorper quis.
+        Finding a job can be very difficult. Keeping track of what jobs you've
+        applied for, where you are in the process, and preparing for interviews
+        takes a lot of time and energy. <strong>Job.Hunt.Works</strong> makes
+        this easier!
+      </p>
+      <p>
+        <img className="screenshot" src="/images/screenshots/full.png"/>
+      </p>
+      <p>
+        This site is not a jobs board. Instead, it's a set of tools to help you
+        manage the process of job hunting. Keep track of jobs you're interested
+        in, and see what's coming up next at a glance.
+      </p>
+      <p>
+        <img className="screenshot" src="/images/screenshots/job-list.png"/>
+      </p>
+      <p>
+        Manage events and prepare notes for your interviews. You'll always know
+        what questions you want to ask, and what answers you want to give.
+      </p>
+      <p>
+        <img className="screenshot" src="/images/screenshots/milestone.png"/>
+      </p>
+      <p>
+        <strong>Job.Hunt.Works</strong> also nudges you to make sure you're
+        keeping your spirits up. Recommended activities and a mood tracker help
+        you stay focused and positive.
+      </p>
+      <p>
+        <img className="screenshot" src="/images/screenshots/helpers.png"/>
+      </p>
+      <p>
+        <strong>Sign up today for free! No credit card required!</strong>
       </p>
       <p className="button-group">
         <Link to="/signup/" className="btn btn-lg btn-primary">Sign Up!</Link>
         <Link to="/login/"  className="btn btn-lg btn-success">Login!</Link>
-      </p>
-      <p>
-        Morbi nec tellus dictum, cursus dui id, tempor eros. Suspendisse aliquam elit sit amet diam vulputate aliquet. Vestibulum laoreet cursus pellentesque. Curabitur interdum feugiat rutrum. Cras nec porta nunc. Cras aliquet, purus vel tincidunt porttitor, lectus est sagittis nisl, in imperdiet libero elit non augue. Aliquam vestibulum posuere turpis id congue. Aliquam porttitor venenatis vulputate.
-      </p>
-      <p>
-        Curabitur id vehicula metus. In sit amet ipsum at metus fringilla scelerisque sed a nibh. Suspendisse a nunc non libero pretium luctus. Sed mollis scelerisque dolor nec ornare. Vivamus vel nisl in orci bibendum euismod. Integer at sagittis dolor. Fusce sed vulputate diam, vitae fermentum ante. Donec urna elit, pulvinar quis arcu eu, pulvinar hendrerit dui. Nulla vitae fermentum purus, et aliquet quam. Nam eu enim lorem. Morbi tincidunt fermentum arcu, porta posuere tellus condimentum sed. Vestibulum ut bibendum orci, et facilisis lorem. Sed in mauris cursus ex bibendum congue nec at eros.
-      </p>
-      <p>
-        Aenean interdum mauris nec dolor lobortis, a venenatis enim mollis. Curabitur ut massa faucibus, euismod diam quis, ullamcorper arcu. Curabitur sed semper erat. Mauris non massa viverra, ullamcorper metus in, elementum dui. Phasellus vulputate arcu ut dui cursus, vitae malesuada diam dictum. Mauris vitae nulla luctus, fringilla massa vel, hendrerit tellus. Vestibulum in convallis eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-      </p>
-      <p>
-        Pellentesque eget lacus dui. Pellentesque eu diam et mauris vestibulum hendrerit at vel erat. Proin nec consectetur nunc, in facilisis elit. Vestibulum in turpis sit amet nisi fermentum finibus. Vestibulum laoreet, massa nec pulvinar vehicula, quam felis hendrerit urna, sit amet vulputate metus odio et ligula. Duis suscipit diam eget tellus vulputate, ut fringilla neque pulvinar. Vestibulum vehicula lorem vitae dolor tempor euismod. Nullam sit amet tortor vestibulum metus pretium sagittis. Maecenas nec odio at eros pulvinar elementum nec ac diam. Donec sollicitudin vel est nec facilisis. Fusce nibh metus, lacinia consectetur fermentum ac, vestibulum non ipsum. Nulla facilisi.
       </p>
     </div>);
   }
