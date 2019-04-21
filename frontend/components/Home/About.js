2a8d7b76f915320cdd56afdac84baecfb4c42ef3
Bill Hunt <bill@krues8dr.com>
2019-04-21 17:04:32 -0400

    Fix up about page. Add link to issues page in footer.

--- frontend/components/Home/About.js
+++ frontend/components/Home/About.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function About() {
   return(
--- frontend/components/modules/Footer.js
+++ frontend/components/modules/Footer.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -11,6 +13,10 @@ export default function Footer () {
           </li>
           <li className="nav-item">
             <Link to="/tos/" className="nav-link">Terms of Service</Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" target="_blank"
+            href="https://github.com/krues8dr/jobs/issues/new">Let us know if something isn't working!</a>
           </li>
         </ul>
       </nav>
