1cb78c2722189e0eef409a28469d9d84786feec3
Bill Hunt <bill@krues8dr.com>
2019-04-21 16:31:05 -0400

    Cleaning up styles. Adding About & TOS pages.

---
+++ frontend/components/Home/About.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+export default function About() {
+  return(
+    <section className="about">
+      <h2>About Job.Hunt.Works</h2>
+      <p>
+        This site is not a jobs board. Instead, it's a set of tools to help you
+        manage the process of job hunting. Keep track of jobs you're interested
+        in, and see what's coming up next at a glance.
+      </p>
+      <p>
+        <img className="screenshot" src="/images/screenshots/job-list.png"/>
+      </p>
+      <p>
+        Manage events and prepare notes for your interviews. You'll always know
+        what questions you want to ask, and what answers you want to give.
+      </p>
+      <p>
+        <img className="screenshot" src="/images/screenshots/milestone.png"/>
+      </p>
+      <p>
+        <strong>Job.Hunt.Works</strong> also nudges you to make sure you're
+        keeping your spirits up. Recommended activities and a mood tracker help
+        you stay focused and positive.
+      </p>
+      <p>
+        <img className="screenshot" src="/images/screenshots/helpers.png"/>
+      </p>
+      <p>
+        <strong>Sign up today for free! No credit card required!</strong>
+      </p>
+      <p className="button-group">
+        <Link to="/signup/" className="btn btn-lg btn-primary">Sign Up!</Link>
+        <Link to="/login/"  className="btn btn-lg btn-success">Login!</Link>
+      </p>
+    </section>
+  );
+}
---
+++ frontend/components/Home/TermsOfService.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+export default function TermsOfService() {
+  return(
+    <section className="tos">
+      <h2>Terms of Service</h2>
+      <p>
+        By using this website, you agree to the following:
+      </p>
+      <p>
+        We don't plan on giving away or selling the data that you give us. We'll
+        keep it as safe as we can, like a secret. If we change our minds for any
+        reason, we'll let you know as early as we can.
+      </p>
+      <p>
+        Although we try hard to protect your data, it's possible that at some
+        point your data may be accidentally be released to another party. If
+        that happens, we'll try our best to let you know what happened.
+      </p>
+      <p>
+        You hold us (Job.Hunt.Works) and the owner (Bill Hunt) harmless if
+        anything like that or otherwise not-good should happen, and you won't
+        sue us or anything.
+      </p>
+    </section>
+  );
+}
